60eef6681d9c850a22a5733dc5d2eb7ea162df0c
Ganesh8999 <Ganesh8999@github.com>
2020-11-06 17:06:45 +0530

    login and signup

--- RedditApp/reddit-app-clone/src/app/app.module.ts
+++ RedditApp/reddit-app-clone/src/app/app.module.ts
@@ -1,20 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { HttpClientModule } from "@angular/common/http";
+import { LoginComponent } from "./auth/login/login.component";
+import { NgxWebstorageModule } from "ngx-webstorage";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SignupComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxWebstorageModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ RedditApp/reddit-app-clone/src/app/auth/login/login.component.ts
@@ -0,0 +1,22 @@
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      username: new FormControl("username", Validators.required),
+      password: new FormControl("password", Validators.required),
+    });
+  }
+
+  login() {}
+}
